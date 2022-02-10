8bb221af4a0802c0b1d1216795a51c0766edb07e
willianchu <willianchu@hotmail.com>
2022-02-09 22:30:07 -0800

    1_queries added

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -36,6 +36,7 @@ CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
+  reservation_id INTEGER UNIQUE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
