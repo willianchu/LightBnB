200abd97d88f7f160838de2636af2151aaa9a8d9
willianchu <willianchu@hotmail.com>
2022-02-10 20:51:57 -0800

    property adding to the database query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -160,7 +160,7 @@ const getAllProperties = function(options, limit = 10) {
     } else {
       queryString += ` AND`;
     }
-    queryParams.push(`%${options.owner_id}%`);
+    queryParams.push(options.owner_id * 1);
     queryString += ` owner_id = $${queryParams.length} `;
   }
   if (options.minimum_price_per_night) {
@@ -225,6 +225,23 @@ const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  console.log("<<<",property);
+  console.log(">>>",properties);
+  
+  return pool
+    .query(`
+    INSERT INTO properties ( owner_id,title,description, thumbnail_photo_url,
+      cover_photo_url,cost_per_night, street, city,province,post_code, country,
+      parking_spaces,number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log('Error adding user', err.message);
+    });
 };
 exports.addProperty = addProperty;
+// const propertyId = Object.keys(properties).length + 1;
+// property.id = propertyId;
+// properties[propertyId] = property;
+// return Promise.resolve(property);
